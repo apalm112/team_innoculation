7111b891cf0485ead98ef5de2388e66c7e6e18cc
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-02 13:50:23 -0700

    got iife to work

--- scripts/model/map.js
+++ scripts/model/map.js
@@ -1,16 +1,19 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: {lat: 47.4235, lng: -120.3103},
-    zoom: 6,
-    panControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_LEFT
-    },
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.LARGE,
-      position: google.maps.ControlPosition.RIGHT_CENTER
-    },
-    disableDoubleClickZoom: true
-  });
-};
+(function(module){
+  var mapRender = {};
 
-initMap();
+  mapRender.initMap = function() {
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: {lat: 47.4235, lng: -120.3103},
+      zoom: 6,
+      panControlOptions: {
+        position: google.maps.ControlPosition.BOTTOM_LEFT
+      },
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      },
+      disableDoubleClickZoom: true
+    });
+  };
+  module.mapRender = mapRender;
+})(window);
