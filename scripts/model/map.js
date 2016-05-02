16a26ea5bc10477ec58445d1f6d1cb34f78fbb23
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-02 15:35:51 -0700

    took out iife module

--- scripts/model/map.js
+++ scripts/model/map.js
@@ -1,19 +1,21 @@
-(function(module){
-  var mapRender = {};
 
-  mapRender.initMap = function() {
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-      center: {lat: 47.4235, lng: -120.3103},
-      zoom: 6,
-      panControlOptions: {
-        position: google.maps.ControlPosition.BOTTOM_LEFT
-      },
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.LARGE,
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      },
-      disableDoubleClickZoom: true
-    });
-  };
-  module.mapRender = mapRender;
-})(window);
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: {lat: 47.4235, lng: -120.3103},
+    zoom: 6,
+    panControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT
+    },
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE,
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    },
+    disableDoubleClickZoom: true
+  });
+
+};
+
+function hideMap() {
+  $('#map-container').hide();
+};
