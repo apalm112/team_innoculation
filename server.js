b6ebe3d7a33bf057501d02076e7f935545f311ed
Dustin McBride <dustinbm@gmail.com>
2016-05-02 11:15:17 -0700

    pulling from socrata and saving to firedb, woot

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 var requestProxy = require('express-request-proxy'),
   express = require('express'),
   http = require('https'),
+  Firebase = require('firebase'),
   port = process.env.PORT || 3000,
   app = express();
 
+fireDataBase = new Firebase('https://intense-heat-7080.firebaseio.com');
+fireDataBase.remove();
+var url = 'https://data.wa.gov/resource/rfq4-2k5i.json?' + '$limit=3000&' + '$$app_token=' + process.env.WA_DATA_TOKEN;
+
 app.use(express.static('./'));
 
-app.get('*', function(request, response) {
+app.get('*', function (request, response) {
   console.log('New request:', request.url);
-  response.sendFile('index.html', {root: '.'});
+  response.sendFile('index.html', {
+    root: '.'
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Server started on port ' + port + '!');
 });
 
-var url = 'https://data.wa.gov/resource/rfq4-2k5i.json?' + '$limit=3000&' + '$$app_token=' + process.env.WA_DATA_TOKEN;
-
-console.log(url);
-
-http.get(url, function(res) {
+http.get(url, function (res) {
   var body = '';
-
   res.on('data', function (chunk) {
     body += chunk;
   });
-
-  res.on('end', function() {
+  res.on('end', function () {
     var socrataResponse = JSON.parse(body);
-    console.log(socrataResponse.length);
+    socrataResponse.map(function (school) {
+      fireDataBase.push(school);
+    });
+  }).on('error', function (e) {
+    console.log('Got an error: ');
   });
-}).on('error', function(e) {
-  console.log('Got an error: ');
 });
