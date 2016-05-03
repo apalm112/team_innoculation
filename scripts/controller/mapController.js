7f0826de5883140b4bfd41e6aea79caf097d3cc8
Dustin McBride <dustinbm@gmail.com>
2016-05-03 14:02:44 -0700

    makers on maps mofo's....maybe

--- scripts/controller/mapController.js
+++ scripts/controller/mapController.js
@@ -1,7 +1,7 @@
-(function(module) {
+(function (module) {
   var mapController = {};
 
-  mapController.index = function(ctx, next) {
+  mapController.index = function (ctx, next) {
 
     var schoolId, allLatLng, allMarkers, schoolName = [];
 
@@ -18,22 +18,41 @@
     next();
   };
 
-  mapController.findSchools = function(ctx, next) {
+  mapController.findSchools = function (ctx, next) {
 
     var ref = new Firebase('https://intense-heat-7080.firebaseio.com/');
 
-    ref.on('value', function(snapshot) {
+    ref.once('value', function (snapshot) {
       ctx.schools = snapshot.val();
       next();
-    }, function(errorObject) {
+    }, function (errorObject) {
       console.log('The read failed: ' + errorObject.code);
     });
   };
 
-  mapController.renderSchools = function(ctx, next) {
+  mapController.renderSchools = function (ctx, next) {
 
-    $(ctx.schools.schools).map(function() {
-      console.log(ctx.schools.schools);
+    keys = Object.keys(ctx.schools.schools);
+    schoolArray = [];
+    keys.map(function (k) {
+      schoolArray.push({
+        key: k,
+        latLng: {
+          lat: ctx.schools.schools[k].lat,
+          lng: ctx.schools.schools[k].lng
+        }
+      });
+    });
+
+    schoolArray.map(function (school) {
+      var marker = new google.maps.Marker({
+
+        position: school.latLng,
+        title: school.key
+      });
+
+      // To add the marker to the map, call setMap();
+      marker.setMap(map);
     });
   };
 
