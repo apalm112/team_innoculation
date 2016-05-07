d76d1a410b435d611ed197c8f3e79a1a47261c64
Dustin McBride <dustinbm@gmail.com>
2016-05-07 00:56:44 -0700

    performance boost, now with better MVC

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,5 +1,5 @@
 
-page('/', homeController.index, indexView.initAutocomplete, indexView.geolocate);
+page('/', homeController.index, indexView.initAutocomplete, indexView.geolocate, School.fetchAll);
 page('/map', mapController.findSchools, mapController.index, mapController.renderSchools, mapView.initAutocomplete);
 page('/map/lat/:lat/lng/:lng', mapController.findSchools, mapController.index, mapController.renderSchools, mapView.initAutocomplete);
 page('/about', aboutController.index);
